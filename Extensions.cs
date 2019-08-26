019ae391ac00936176f10a14c5320e010586dc3e
Tim Seidel <timme.97@gmx.de>
2019-08-26 21:52:28 +0200

    Added Extensions/Util class

---
+++ Extensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BierPongTurnier
+{
+    public class Extensions
+    {
+        public static void Shuffle(IList<string> list, Random random)
+        {
+            int n = list.Count;
+            int k;
+            string value;
+            while (n > 1)
+            {
+                n--;
+                k = random.Next(n + 1);
+                value = list[k];
+                list[k] = list[n];
+                list[n] = value;
+            }
+        }
+    }
+}
